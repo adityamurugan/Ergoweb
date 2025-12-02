b9ec752823a9e75a5abcc5e9d9fe273b9daea777
adityamurugan <48570983+adityamurugan@users.noreply.github.com>
2025-12-02 12:28:52 -0500

    layout update

--- rula-eval/src/app/layout.tsx
+++ rula-eval/src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { GeistSans } from "@geist/font/sans";
-import { GeistMono } from "@geist/font/mono";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import "./globals.css";
-
-const geistSans = GeistSans({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = GeistMono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +14,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html
+      lang="en"
+      className={`${GeistSans.variable} ${GeistMono.variable}`}
+    >
+      <body>{children}</body>
     </html>
   );
 }
